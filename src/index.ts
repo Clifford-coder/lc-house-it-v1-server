1b73758579235c7289c1a7eeae05077ab2d64aa8
LordCliff77 <cliffowusu2727@gmail.com>
2021-05-12 09:12:21 +0100

    Restructed code and added apollo-server to use graphql

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,12 @@
 import express from 'express';
-import bodyParser from 'body-parser';
+import { ApolloServer } from 'apollo-server-express';
 
-import { listings } from './listings';
+import { typeDefs, resolvers } from './graphql';
 
 const app = express();
 const port = 8994;
-
-app.use(bodyParser.json());
-
-app.get('/listings', (_req, res) => res.send(listings));
-
-app.post('/delete-listings', (req, res) => {
-	const id: string = req.body.id;
-	for (let i = 0; i < listings.length; i++) {
-		if (listings[i].id === id) {
-			return res.send(listings.splice(i, 1));
-		}
-	}
-
-	res.send(`failed to delete item with ${id}`);
-});
+const server = new ApolloServer({ typeDefs, resolvers });
+server.applyMiddleware({ app, path: '/api' });
 
 app.listen(port);
 
